e5dc758400fc3afd165d1793dac18b35c8948927
Kevyn <kevynsilveira0610@gmail.com>
2023-08-07 13:39:49 -0300

    Finalizado o código para busca arquivo.

--- main.py
+++ main.py
@@ -3,68 +3,63 @@ import zipfile
 import tkinter as tk
 import customtkinter as ctk
 from tkinter import filedialog
+from tkinter import messagebox
 
-arquivos_encontrados = []
+arquivos_encontrados = []#Lista de arquivos
 
-def buscar_arquivos_por_termo():
-    termo = entry_searchFile.get()
-    diretorio = entry_select.get()
-
+def search(): # Busca os arquivos por código
+    code = entry_searchFile.get()
+    directory = entry_select.get()
     global arquivos_encontrados
-    for nome_arquivo in os.listdir(diretorio):
-        if termo.lower() in nome_arquivo.lower():
-            arquivos_encontrados.append(os.path.join(diretorio, nome_arquivo))
+    for file in os.listdir(directory):
+        if code.lower() in file.lower():
+            arquivos_encontrados.append(os.path.join(directory, file))
     return arquivos_encontrados
 
-def zipar_arquivos():
+def zip():
     global arquivos_encontrados
-    destino = entry_destiny.get()
-    with zipfile.ZipFile(destino, 'w') as zip_file:
-        for arquivo in arquivos_encontrados:
-            zip_file.write(arquivo, os.path.basename(arquivo))
+    filetypes = [('Arquivos', '*.zip')]
+    destiny = filedialog.asksaveasfilename(defaultextension=".zip", initialfile="Arquivos", filetypes=filetypes)
+    with zipfile.ZipFile(destiny, 'w') as zip_file:
+        for file in arquivos_encontrados:
+            zip_file.write(file, os.path.basename(file))
 
-def selecionar_pasta_busca():
+def select():
     diretorio = filedialog.askdirectory()
     entry_select.delete(0, tk.END)
     entry_select.insert(0, diretorio)
 
-def selecionar_pasta_destino():
-    filetypes = [('Arquivos', '*.zip')]
-    destino_zip = filedialog.asksaveasfilename(defaultextension=".zip", initialfile="Arquivos", filetypes=filetypes)
-    entry_destiny.delete(0, tk.END)
-    entry_destiny.insert(0, destino_zip)
-
-
-def executa():
-    buscar_arquivos_por_termo()
-    zipar_arquivos()
-    selecionar_pasta_destino()
-
+def execute():
+    try:
+        path = entry_select.get()
+        searchfile = entry_searchFile.get()
+        if(path!="" and searchfile!= ""):
+            search()
+            zip()
+        else:
+            messagebox.showerror("ATENÇÃO", "Preenca todos os campos")
+    except Exception as e:
+        messagebox.showerror("ATENÇÃO", f"Ocorreu um erro: {e}")
 
 frame = ctk.CTk()
-frame.geometry("200x300")
+frame.geometry("200x200")
 frame.title("Busca arquivos")
 frame.resizable(False, False)
 
-button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30, command=selecionar_pasta_busca)
+button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30, command=select)
 button_select.place(x=25, y=10)
 
 entry_select = ctk.CTkEntry(master=frame, width=150, height=30)
 entry_select.place(x=25, y=50)
 
-button_destiny = ctk.CTkButton(master=frame, text= "Diretório destino", width=150, height=30, command=selecionar_pasta_destino)
-button_destiny.place(x=25, y=90)
-
-entry_destiny = ctk.CTkEntry(master=frame, width=150, height=30)
-entry_destiny.place(x=25, y=130)
-
 label_searchFile = ctk.CTkLabel(master=frame,text="Código", width=150, height=30)
-label_searchFile.place(x=25, y=160)
+label_searchFile.place(x=25, y=90)
 
 entry_searchFile = ctk.CTkEntry(master=frame, width=150, height=30)
-entry_searchFile.place(x=25, y=190)
+entry_searchFile.place(x=25, y=120)
+entry_searchFile.configure(justify="center")
 
-button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30, command=executa)
-button_execute.place(x=25, y=260)
+button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30, command=execute)
+button_execute.place(x=25, y=160)
 
 frame.mainloop()
